04d4d43fb03399b80ae8103bc586369e2881203e
laura <ljmanor14@gmail.com>
2019-03-08 11:46:33 -0600

    summa actually works for legaltldr

--- run_lexrank.py
+++ run_lexrank.py
@@ -4,6 +4,8 @@ import logging
 from rouge import Rouge
 from nltk.tokenize import sent_tokenize
 import spacy
+import re
+import pandas as pd
 import pytextrank
 
 
@@ -22,36 +24,82 @@ if __name__ == '__main__':
     for thing in data.items:
         count += 1
         stuff = data.items[thing]
-        text = stuff['text'].replace('\n', '')
+
+        text = stuff['text']
+
+        for match in re.finditer("\.”", text):
+            index = match.start()
+            # print(match)
+
+            # print(text)
+            text = text[:index] + "”." + text[index+2:]
+            # print(text)
+
+
+        for match in re.finditer("[a-z]\.[A-Z]", text):
+            index = match.start()
+            # print(match)
+
+            # print(text)
+            text = text[:index+2] + " " + text[index+2:]
+            # print(text)
+
+        text = text.replace('&nbsp', ' ').replace('\n', ' ').lower()
+
         ratio = .2
+        uid = stuff['uid']
 
-
+        spacy = True
 
         #nltk
         tokenized = sent_tokenize(text)
-        input = "\n".join(tokenized)
+        num_sents = len(tokenized)
+        # if num_sents == 1:
+        if spacy:
+            spacy = False
+            tokenized = list(nlp(text).sents)
+            num_sents = len(tokenized)
+            if num_sents != 1:
+                input = "\n".join(sent.text for sent in tokenized)
+                print("SPACY")
+            else:
+                hypes.append(hyp)
+                refs.append(stuff['ref'])
+                print(f'number: {count, len(hypes)} | {uid} | #sent: {num_sents} | ratio: {ratio} | BREAK')
 
-        spacy = True
-        while ratio < 1:
-            hyp = summarize(input)
+                ratio = 1.1
+        else:
+            input = "\n ".join(tokenized)
+
+
+
+        while ratio <= 1.0:
+
+            hyp = summarize(input, ratio)
             if len(hyp) > 0:
                 hypes.append(hyp)
                 refs.append(stuff['ref'])
-                print(count)
+                print(f'number: {count, len(hypes)} | Success {uid} | #sent: {num_sents} | ratio: {ratio} | spacy: {spacy}')
                 break
             else:
 
-                ratio += .05
-                if ratio > .8 and spacy:
-                    # spacy
-                    tokenized = list(nlp(text).sents)
-                    input = "\n".join(sent.text for sent in tokenized)
+                ratio = round(ratio+.1,1)
+                # if ratio > .8 and spacy is True:
+                #     # spacy
+                #     tokenized = list(nlp(text).sents)
+                #     num_sents = len(tokenized)
+                #     input = "\n".join(sent.text for sent in tokenized)
+                #     ratio = .2
+                #
+                #     spacy = False
+                # if int(ratio) >= 1.05:
+                #     hypes.append(input)
+                #     refs.append(stuff['ref'])
+                #     print(f'number: {count, len(hypes)} | Fail {uid} | #sent: {num_sents} | ratio: {ratio} | ')
 
-                    spacy = False
-                # print(count)
 
+    print(len(hypes))
     rouge = Rouge()
-    scores = rouge.get_scores(hypes, refs)
-    print(len(scores))
+    scores = rouge.get_scores(hypes, refs, avg=True)
+    print(scores)
 
-    pass
