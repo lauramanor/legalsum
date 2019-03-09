70efd84ec6e3f16cfbbad3e34f6ef9b5cbada330
laura <ljmanor14@gmail.com>
2019-03-09 10:48:43 -0600

    cleaned up oop

--- data/__init__.py
+++ data/__init__.py
@@ -36,9 +36,9 @@ class Summaries():
         self.check_dir()
         reader = csv.DictReader(open(self.files[file]))
         for row in reader:
-            self.items.append(Summary(row))
+            self.items.append(Summary(row, file=file))
 
-            pass
+        logging.info(f'Loaded {file}')
 
     def check_dir(self, check=None):
         if not check:
@@ -51,57 +51,85 @@ class Summaries():
         logging.debug(f'Checking {check}')
         logging.debug(', '.join(self.files))
 
-    def textrank(self, spacy=False):
+    def textrank(self, use_spacy=False):
         hypes = []
         refs = []
+        refs_dirty = []
         for item in self.items:
-            if item.quote.nsents(spacy) <= 1:
-                hypes.append(item.quote.clean)
-                refs.append(item.ref.clean)
-                print(item.quote.clean)
-            else:
-                for x in range(2, 11):
-                    ratio = x * .1
-                    hyp = summarize("\n ".join(item.quote.sents(spacy)), ratio)
-                    if len(hyp) > 0:
-                        hypes.append(hyp)
-                        refs.append(item.ref.clean)
-                        break
+            if item.ref:
 
-        print(len(hypes), spacy)
+                if item.quote.nsents(use_spacy) <= 1:
+                    hypes.append(item.quote.clean)
+                    refs.append(item.ref.clean)
+                    refs_dirty.append(item.ref.original)
+                    # print(item.quote.clean)
+                else:
+                    for x in range(2, 11):
+                        ratio = x * .1
+                        hyp = summarize("\n ".join(item.quote.sents(use_spacy)), ratio)
+                        if len(hyp) > 0:
+                            hypes.append(hyp)
+                            refs.append(item.ref.clean)
+                            refs_dirty.append(item.ref.original)
+
+                            break
+
+        # print(len(hypes), use_spacy)
+        #
+        # print(rouge.get_scores(hypes, refs_dirty, avg=True))
 
         return rouge.get_scores(hypes, refs, avg=True)
 
 class Summary:
 
-    def __init__(self, row):
+    def __init__(self, row, file):
+        self.row = row
         self.uid = row['uid']
         self.quote = Document(row['text'])
-        self.ref = Document(row['summary'])
-        self.id = row['id']
-        self.title = row['title']
+        if file == "tldrlegal":
+            self.ref = Document(row['summary'])
+            self.id = row['id']
+            self.title = row['title']
+            self.name = row['name']
+        elif file == "tosdr_annotated":
+            self.note = row["note"]
+            self.urls = row["urls"]
+            self.quote_doc = row["quoteDoc"]
+            if self.identify_code("1"):
+                self.ref = Document(self.identify_code("1"))
+            else:
+                self.ref = False
 
+    def identify_code(self, x):
+        for key in self.row.keys():
+            if key.endswith('_code'):
+                if x in self.row[key]:
+                    # print(key[:-5])
+                    return self.row[key[:-5]]
 
 class Document:
 
     def __str__(self):
         return self.clean
 
-    def __init__(self, text):
+    def __init__(self, text, code=None):
         self.original = text
         self.clean = self.clean_text()
 
         self.sents_nltk = self.get_sentences()
         self.nsents_nltk = len(self.sents_nltk)
 
-        self.sents_spacy = self.get_sentences(spacy=True)
+        self.sents_spacy = self.get_sentences(use_spacy=True)
         self.nsents_spacy = len(self.sents_spacy)
 
         self.ngrams = self.get_ngrams()
         self.nwords = word_tokenize(self.clean)
 
+        self.code = code
+
     def clean_text(self):
         text = self.original
+        # print(text)
         for match in re.finditer("\.”", text):
             index = match.start()
             text = text[:index] + "”." + text[index + 2:]
@@ -112,12 +140,12 @@ class Document:
 
         return text.replace('&nbsp', ' ').replace('\n', ' ').lower()
 
-    def get_sentences(self, text=None, spacy=False):
+    def get_sentences(self, text=None, use_spacy=False):
 
         if not text:
             text = self.clean
 
-        if spacy:
+        if use_spacy:
             tokenized = list(nlp(text).sents)
             return [sent.text for sent in tokenized]
         else:
@@ -135,14 +163,14 @@ class Document:
 
         return grams
 
-    def nsents(self, spacy=False):
-        if spacy:
+    def nsents(self, use_spacy=False):
+        if use_spacy:
             return self.nsents_spacy
         else:
             return self.nsents_nltk
 
-    def sents(self, spacy=False):
-        if spacy:
+    def sents(self, use_spacy=False):
+        if use_spacy:
             return self.sents_spacy
         else:
             return self.sents_nltk
--- run_lexrank.py
+++ run_lexrank.py
@@ -13,64 +13,24 @@ nlp = spacy.load('en_core_web_sm')
 logging.basicConfig(level=logging.DEBUG)
 
 
-if __name__ == '__main__':
-    # data = Summaries(['tldrlegal','tosdr_annotated'])
-    data = Summaries(['tldrlegal'])
+def print_scores(scores):
+    toprint = ""
+    for key in scores.keys():
+        toprint += str(round(scores[key]['f']*100,2)) + " & "
 
-    # hypes = []
-    # refs = []
-    # count = 0
-    # 
-    # for thing in data.items:
-    #     count += 1
-    #     stuff = data.items[thing]
-    # 
-    #     text = stuff['text']
-    # 
-    # 
-    # 
-    #     ratio = .2
-    #     uid = stuff['uid']
-    # 
-    #     spacy = True
-    # 
-    #     #nltk
-    #     tokenized = sent_tokenize(text)
-    #     num_sents = len(tokenized)
-    #     # if num_sents == 1:
-    #     if spacy:
-    #         spacy = False
-    #         tokenized = list(nlp(text).sents)
-    #         num_sents = len(tokenized)
-    #         if num_sents != 1:
-    #             input = "\n".join(sent.text for sent in tokenized)
-    #             print("SPACY")
-    #         else:
-    #             hypes.append(hyp)
-    #             refs.append(stuff['ref'])
-    #             print(f'number: {count, len(hypes)} | {uid} | #sent: {num_sents} | ratio: {ratio} | BREAK')
-    # 
-    #             ratio = 1.1
-    #     else:
-    #         input = "\n ".join(tokenized)
-    # 
-    # 
-    # 
-    #     while ratio <= 1.0:
-    # 
-    #         hyp = summarize(input, ratio)
-    #         if len(hyp) > 0:
-    #             hypes.append(hyp)
-    #             refs.append(stuff['ref'])
-    #             print(f'number: {count, len(hypes)} | Success {uid} | #sent: {num_sents} | ratio: {ratio} | spacy: {spacy}')
-    #             break
-    #         else:
-    # 
-    #             ratio = round(ratio+.1,1)
-    # 
-    # 
-    # print(len(hypes))
-    # rouge = Rouge()
-    # scores = rouge.get_scores(hypes, refs, avg=True)
-    print(data.textrank())
-    print(data.textrank(spacy=True))
+    print(toprint)
+
+if __name__ == '__main__':
+    data = Summaries(['tldrlegal','tosdr_annotated'])
+    print_scores(data.textrank())
+    print_scores(data.textrank(use_spacy=True))
+
+
+    data = Summaries(['tosdr_annotated'])
+    print_scores(data.textrank())
+    print_scores(data.textrank(use_spacy=True))
+
+
+    data = Summaries(['tldrlegal'])
+    print_scores(data.textrank())
+    print_scores(data.textrank(use_spacy=True))
